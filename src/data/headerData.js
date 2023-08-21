a09827f9aec0c15f2fcfe345738d27f685a7fd3c
Manan Patel <mananpatel6902@gmail.com>
2023-08-21 19:41:37 -0400

    Changing profile picture

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,4 +1,5 @@
 import resume from "../assets/pdf/resume.pdf";
+import profilePic from "../assets/png/profile-pic.jpg";
 
 export const headerData = {
   name: "Manan Patel",
@@ -6,6 +7,6 @@ export const headerData = {
   desciption:
     "A Senior at the University of Tennessee Knoxville pursuing a Bachelor of Science in Computer Science, I am passionate about building software and exploring different career fields within the industry. I am eager to further develop my skills and make a meaningful impact in the field.",
   image:
-    "https://media.licdn.com/dms/image/C5603AQFUYRqBlgph1Q/profile-displayphoto-shrink_800_800/0/1628295601944?e=1692230400&v=beta&t=6RnHPHCW62JHtIsiQoZsYf_c8Wf0te6BI__dhz1DjgA",
+    profilePic,
   resumePdf: resume,
 };
